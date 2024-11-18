ce8b44e85172218d63dcc8e3df6b96081038a601
arjunsuresh <arjunsuresh@users.noreply.github.com>
2024-11-18 00:59:24 +0000

    Deployed 08eb802c with MkDocs version: 1.6.1

---
+++ javascripts/config.js
@@ -0,0 +1,7 @@
+const results_version="v4.1";
+var repo_owner="GATEOverflow";
+var repo_branch="main";
+var repo_name="inference_results_v4.1";
+const dbVersion ="64";
+const default_category ="datacenter";
+const default_division ="closed";
